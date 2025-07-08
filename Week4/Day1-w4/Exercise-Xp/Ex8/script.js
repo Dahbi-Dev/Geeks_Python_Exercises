6f21804d12643557f4b518c72371640aa04ce0f5
Dahbi-Dev <houssamdahbi007@gmail.com>
2025-07-08 15:58:37 +0100

    complete Ex8

---
+++ Week4/Day1-w4/Exercise-Xp/Ex8/script.js
@@ -0,0 +1,21 @@
+function makeJuice(size) {
+    const ingredients = [];
+
+    function addIngredients([ing1, ing2, ing3]) {
+        ingredients.push([ing1, ing2, ing3]);
+        document.getElementById("message").textContent = `The client wants a
+         ${size} juice, containing ${ing1}, 
+        ${ing2}, ${ing3}`;
+    }
+
+    function displayJuice() {
+        document.getElementById("display").textContent = `The client wants a
+        ${size} juice, containing ${ingredients.join(", ")}`;
+    }
+
+    addIngredients(["chocolate", "caramel", "apple"]);
+    addIngredients(["banana", "mango", "orange"]);
+    displayJuice();
+}
+
+makeJuice("Small");
