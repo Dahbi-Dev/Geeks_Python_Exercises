50174efcdf19b0b95a5e62992ea8a1860a5837e6
Dahbi-Dev <houssamdahbi007@gmail.com>
2025-07-09 15:47:54 +0100

    complete xp Week4 Day 2

--- Week4/Day2-w4/ExercisesXP/Ex6/script.js
+++ Week4/Day2-w4/ExercisesXP/Ex6/script.js
@@ -1,13 +1,52 @@
-document.getElementById("Ex6").textContent = "Exercise 6"
-// Exercise 6 : Fortune teller
+const students = [{
+        name: "Ray",
+        course: "Computer Science",
+        isPassed: true
+    },
+    {
+        name: "Liam",
+        course: "Computer Science",
+        isPassed: false
+    },
+    {
+        name: "Jenner",
+        course: "Information Technology",
+        isPassed: true
+    },
+    {
+        name: "Marco",
+        course: "Robotics",
+        isPassed: true
+    },
+    {
+        name: "Kimberly",
+        course: "Artificial Intelligence",
+        isPassed: false
+    },
+    {
+        name: "Jamie",
+        course: "Big Data",
+        isPassed: false
+    }
+];
 
-function info ({numberChildren ,partner, Geo, Jobtitle }){
-    console.log(`You will be a ${Jobtitle} in ${Geo}, and married to ${partner} with ${numberChildren} kids.`)
-}
-info({
-    partner: "Ledia",
-    numberChildren : 5,
-    Jobtitle: "Full-Stack Developer",
-    Geo: "Casablanca",
+let passedStudents = []
+let congratulations = ""
+students.filter((items) => {
+    if (items.isPassed) {
+        passedStudents.push(items)
+    }
 })
+
+console.log("-----Q1------")
+console.log(passedStudents)
+console.log("-----Q2------")
+
+
+passedStudents.forEach((message) => {
+    if (message.isPassed)
+        congratulations = `Good job ${message.name} ou passed the course in ${message.course}`
+    console.log(congratulations)
+})
+
 
