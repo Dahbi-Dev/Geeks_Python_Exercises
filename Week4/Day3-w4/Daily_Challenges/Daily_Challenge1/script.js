8cf33536a983b9d308f653fa70d7cf3c696fc69b
Dahbi-Dev <houssamdahbi007@gmail.com>
2025-07-10 17:04:18 +0100

    day 3 daily_challenge

--- Week4/Day3-w4/Daily_Challenges/Daily_Challenge1/script.js
+++ Week4/Day3-w4/Daily_Challenges/Daily_Challenge1/script.js
@@ -1,52 +1,28 @@
-const gameInfo = [{
-        username: "john",
-        team: "red",
-        score: 5,
-        items: ["ball", "book", "pen"]
-    },
-    {
-        username: "becky",
-        team: "blue",
-        score: 10,
-        items: ["tape", "backpack", "pen"]
-    },
-    {
-        username: "susy",
-        team: "red",
-        score: 55,
-        items: ["ball", "eraser", "pen"]
-    },
-    {
-        username: "tyson",
-        team: "green",
-        score: 1,
-        items: ["book", "pen"]
-    },
+class Video {
+    constructor(title, uploader, time) {
+        this.title = title
+        this.uploader = uploader
+        this.time = time
+    }
+    
+    
+    watch() {
+        console.log(`${this.uploader} watched all ${this.time} minute of ${this.title} !`)
+    }
+}
+
+const firstVid = new Video("firstTitle", "Houssam", 5)
+firstVid.watch()
+const secondVid = new Video("SecodTitle", "Khalid", 34)
+secondVid.watch()
+
+const videoDataArray = [
+    { title: "Cats Compilation", uploader: "Maya", time: 150 },
+    { title: "JavaScript Crash Course", uploader: "Sam", time: 1800 },
+    { title: "Nature Documentary", uploader: "Lina", time: 2400 },
+    { title: "Workout Routine", uploader: "Tom", time: 900 },
+    { title: "Cooking Pasta", uploader: "Anna", time: 600 }
 ];
 
-// -------------------------------
-
-console.log("---Q1----")
-let usernameArry = []
-gameInfo.forEach((names, index) => {
-    usernameArry.push(`${names.username}!`)
-})
-console.log(usernameArry)
-
-// -------------------------------
-
-console.log("---Q2----")
-let nameOfPlayers = []
-gameInfo.forEach((players => {
-    if (players.score > 5) {
-        nameOfPlayers.push(players.username)
-    }
-}))
-console.log(nameOfPlayers)
-
-// -------------------------------
-
-console.log("---Q3----")
-const totalScore = gameInfo.reduce((acc, total) => (acc + total.score), 0)
-
-console.log(totalScore)
+const videos = videoDataArray.map(vid=> new Video(vid.title, vid.uploader,vid.time))
+videos.forEach(video=> video.watch())
