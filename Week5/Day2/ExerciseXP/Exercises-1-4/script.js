1f1eacccf6c72c57df124652e1d585f65aba349c
Dahbi-Dev <houssamdahbi007@gmail.com>
2025-07-17 10:42:33 +0100

    Exercises XP

--- Week5/Day2/ExerciseXP/Exercises-1-4/script.js
+++ Week5/Day2/ExerciseXP/Exercises-1-4/script.js
@@ -3,85 +3,85 @@
 
 const container = document.getElementById("container")
 
-// const fetchGiphy = async (searchName) => {
-//     container.innerHTML = "";
-//     let limit;
-//     let offset;
+const fetchGiphy = async (searchName) => {
+    container.innerHTML = "";
+    let limit;
+    let offset;
 
-//     if (searchName === 'sun') {
-//         // i change variable value to limit 10 gifs and start position is 2
-//         limit = 10;
-//         offset = 2;
-
-
-//     } else {
-//         console.error("error handling the variables values")
-//     }
-
-//     const loadingDiv = document.createElement("div");
-//     loadingDiv.className = "text-center text-gray-600 p-4";
-//     loadingDiv.textContent = "Loading...";
-//     container.appendChild(loadingDiv);
-
-//     try {
-//         const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchName}&rating=g&limit=${limit}&offset=${offset}&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`)
+    if (searchName === 'sun') {
+        // i change variable value to limit 10 gifs and start position is 2
+        limit = 10;
+        offset = 2;
 
 
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
+    } else {
+        console.error("error handling the variables values")
+    }
 
-//         const data = await response.json();
+    const loadingDiv = document.createElement("div");
+    loadingDiv.className = "text-center text-gray-600 p-4";
+    loadingDiv.textContent = "Loading...";
+    container.appendChild(loadingDiv);
+
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchName}&rating=g&limit=${limit}&offset=${offset}&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`)
 
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-//         container.removeChild(loadingDiv);
-//         console.clear();
+        const data = await response.json();
 
 
 
-
-
-//         data.data.forEach((gif) => {
-//             const box = document.createElement("div");
-//             box.className = "w-full sm:w-1/2 md:w-1/3 p-2 bg-white rounded shadow";
-//             const img = document.createElement("img");
-//             img.src = `https://media1.giphy.com/media/${gif.id}/giphy.gif`;
-//             img.className = "w-full h-auto object-contain";
-//             box.appendChild(img);
-//             container.appendChild(box);
-//             console.log(gif)
-//         });
+        container.removeChild(loadingDiv);
+        console.clear();
 
 
 
-//     } catch (error) {
-//         container.removeChild(loadingDiv);
-//         container.textContent = "Failed to load GIFs.";
-//         console.error("Error fetching data:", error);
-//     }
-// }
 
-// // 🌟 Exercise 3 : Async function
 
-// const starsShips = async () => {
-//     try {
-//         const response = await fetch("https://www.swapi.tech/api/starships/9/")
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
+        data.data.forEach((gif) => {
+            const box = document.createElement("div");
+            box.className = "w-full sm:w-1/2 md:w-1/3 p-2 bg-white rounded shadow";
+            const img = document.createElement("img");
+            img.src = `https://media1.giphy.com/media/${gif.id}/giphy.gif`;
+            img.className = "w-full h-auto object-contain";
+            box.appendChild(img);
+            container.appendChild(box);
+            console.log(gif)
+        });
 
-//         const data = await response.json();
 
-//         console.log(data.result)
 
-//     } catch (error) {
-//         console.error("Error fetching data:", error);
+    } catch (error) {
+        container.removeChild(loadingDiv);
+        container.textContent = "Failed to load GIFs.";
+        console.error("Error fetching data:", error);
+    }
+}
 
-//     }
+// 🌟 Exercise 3 : Async function
 
-// }
-// starsShips()
+const starsShips = async () => {
+    try {
+        const response = await fetch("https://www.swapi.tech/api/starships/9/")
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log(data.result)
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+
+    }
+
+}
+starsShips()
 
 
 // 🌟 Exercise 4: Analyze
