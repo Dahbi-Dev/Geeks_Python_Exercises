1c2fb31d84199d4fc839a55fac274185d763cbef
Dahbi-Dev <houssamdahbi007@gmail.com>
2025-07-26 13:17:04 +0100

    finish Ex6  day 4 XP

---
+++ Week5/Day4/Exercise-XP/Ex6/npm-beginner/app.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello, World!'));
+console.log(chalk.green('This is a simple Node.js application using chalk for colored output.'));
+console.log(chalk.red('Make sure to install the chalk package using npm before running this script.'));
